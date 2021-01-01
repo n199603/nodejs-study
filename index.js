1313aee31608642a73fb9c752f6865b968266be7
n199603 <yutonakamura0320@gmail.com>
2021-01-01 21:21:46 +0900

    イベントループ

---
+++ index.js
@@ -0,0 +1,30 @@
+// グローバルオブジェクト
+console.log("Hello World");
+console.log(__dirname);
+console.log(__filename);
+
+setTimeout(() => {
+  console.log("setTimeout()");
+},100)
+
+console.log("global");
+
+var end = (new Date()).getTime() + 3000;
+while ((new Date()).getTime() < end) {}
+
+// イベントループ
+setTimeout(() => {
+  console.log("setTimeout()");
+},100)
+
+setImmediate(() => {
+  console.log("setImmediate()");
+})
+
+process.nextTick(() => {
+  console.log("nextTick()");
+})
+
+Promise.resolve().then(() => {
+  console.log("Promise.resolve().then()");
+})
