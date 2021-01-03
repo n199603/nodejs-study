3e357cf6f4b734e6a6af5b70cb55054642b430dc
n199603 <yutonakamura0320@gmail.com>
2021-01-03 18:43:50 +0900

    クラス継承(function)

--- class.js
+++ class.js
@@ -2,29 +2,58 @@
 var Car = class {
   constructor(name){
     this.name = name;
-  }
+  };
 
   drive() {
-    console.log("zoom zoom ...")
-  }
-}
+    console.log("zoom zoom ...");
+  };
+};
 
 // 子クラス
 var Lamborghini = class extends Car {
   constructor(name){
     super(name);
-  }
+  };
 
   echo() {
     super.drive();
-  }
+  };
 
   drive() {
-     console.log(`fire ${this.name}`)
-  }
-}
+     console.log(`fire ${this.name}`);
+  };
+};
 
 // 実行
-var car = new Lamborghini("lamborghini")
+var car = new Lamborghini("lamborghini");
+car.echo();
+car.drive();
+
+// function
+var util = require("util");
+
+var Car = function(name) {
+  this.name = name;
+};
+
+Car.prototype.drive = function() {
+  console.log("zoom zoom ...");
+};
+
+var Lamborghini = function(name) {
+  Lamborghini.super_.call(this, name);
+};
+
+util.inherits(Lamborghini, Car);
+
+Lamborghini.prototype.echo = function() {
+  Lamborghini.super_.prototype.drive.call(this);
+};
+
+Lamborghini.prototype.drive = function() {
+  console.log(`fire ${this.name}`);
+};
+
+var car = new Lamborghini("lamborghini");
 car.echo();
 car.drive();
