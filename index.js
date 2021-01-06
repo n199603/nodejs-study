f8ffb55202a7201fc92beb8592127d9104a76e6f
n199603 <yutonakamura0320@gmail.com>
2021-01-06 18:58:59 +0900

    httpサーバー構築

--- index.js
+++ index.js
@@ -182,14 +182,21 @@
 // reader.resume();
 
 // テキストをストリームで書き込み
-var fs = require("fs");
-var path = require("path");
-// var writer = fs.createWriteStream((__dirname, "created.txt"), "utf8");
+// var fs = require("fs");
+// var path = require("path");
+// // var writer = fs.createWriteStream((__dirname, "created.txt"), "utf8");
 
-// writer.end("Hello World!");
+// // writer.end("Hello World!");
 
-var reader = fs.createReadStream(path.join(__dirname, "sample.txt"), "utf8");
-var writer = fs.createWriteStream(path.join(__dirname, "sample-copy5.txt"), "utf8");
+// var reader = fs.createReadStream(path.join(__dirname, "sample.txt"), "utf8");
+// var writer = fs.createWriteStream(path.join(__dirname, "sample-copy5.txt"), "utf8");
 
-reader.pipe(writer);
-reader.resume();
+// reader.pipe(writer);
+// reader.resume();
+
+// HTTPサーバー
+var http = require("http");
+var server = http.createServer((req, res) => {
+  res.end("Hello World");
+})
+server.listen(3000);
